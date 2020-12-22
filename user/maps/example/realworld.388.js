d9c2f13fcb68f6ae54a23ea20e471358f2e5308f
Aditya Agrawal <addyagrawal@gmail.com>
2020-12-22 10:06:59 +0530

    Initial commit

---
+++ user/maps/example/realworld.388.js
@@ -0,0 +1,7 @@
+
+//An extract of address points from the LINZ bulk extract: http://www.linz.govt.nz/survey-titles/landonline-data/landonline-bde
+//Should be this data set: http://data.linz.govt.nz/#/layer/779-nz-street-address-electoral/
+var addressPoints = [
+[23.6165219 , 87.0649332 , "Raniganj Coalfields"],
+[23.7462078 , 85.7125358 , "West Bokaro Coalfields"]]
+
